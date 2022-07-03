fae87d8be0b6b8d4fbb955d9db7d8f2343c6375c
ZuandaBaransyahPutra <zuandabaransyahputra@gmail.com>
2022-07-03 22:57:59 +0700

    add overflow

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,55 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { useRecoilValue } from 'recoil'
-import { modalState } from '../atoms/modalAtom'
-import Banner from '../components/Banner'
-import Header from '../components/Header'
-import Modal from '../components/Modal'
-import Row from '../components/Row'
-import useAuth from '../hooks/useAuth'
-import { Movie } from '../typings'
-import requests from '../utils/requests'
+import Head from 'next/head';
+import Image from 'next/image';
+import { useRecoilValue } from 'recoil';
+import { modalState } from '../atoms/modalAtom';
+import Banner from '../components/Banner';
+import Header from '../components/Header';
+import Modal from '../components/Modal';
+import Row from '../components/Row';
+import useAuth from '../hooks/useAuth';
+import { Movie } from '../typings';
+import requests from '../utils/requests';
 
 interface Props {
-  netflixOriginals: Movie[]
-  trendingNow: Movie[]
-  topRated: Movie[]
-  actionMovies: Movie[]
-  comedyMovies: Movie[]
-  horrorMovies: Movie[]
-  romanceMovies: Movie[]
-  documentaries: Movie[]
+  netflixOriginals: Movie[];
+  trendingNow: Movie[];
+  topRated: Movie[];
+  actionMovies: Movie[];
+  comedyMovies: Movie[];
+  horrorMovies: Movie[];
+  romanceMovies: Movie[];
+  documentaries: Movie[];
 }
 
-const Home = ({ netflixOriginals,
+const Home = ({
+  netflixOriginals,
   actionMovies,
   comedyMovies,
   documentaries,
   horrorMovies,
   romanceMovies,
   topRated,
-  trendingNow, }: Props) => {
+  trendingNow,
+}: Props) => {
+  const { loading } = useAuth();
+  const showModal = useRecoilValue(modalState);
 
-  const { loading } = useAuth()
-  const showModal = useRecoilValue(modalState)
-
-  if (loading) return null
+  if (loading) return null;
 
   return (
-    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
+    <div
+      className={`relative h-screen bg-gradient-to-b lg:h-[140vh] ${
+        showModal && '!h-screen overflow-hidden'
+      }`}
+    >
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals} />
-        <section className='md:space-y-24'>
+        <section className="md:space-y-24">
           <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Thrillers" movies={actionMovies} />
@@ -57,11 +62,10 @@ const Home = ({ netflixOriginals,
       </main>
       {showModal && <Modal />}
     </div>
-  )
-}
+  );
+};
 
-export default Home
-
+export default Home;
 
 export const getServerSideProps = async () => {
   const [
@@ -74,15 +78,15 @@ export const getServerSideProps = async () => {
     romanceMovies,
     documentaries,
   ] = await Promise.all([
-    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
-    fetch(requests.fetchTrending).then((res) => res.json()),
-    fetch(requests.fetchTopRated).then((res) => res.json()),
-    fetch(requests.fetchActionMovies).then((res) => res.json()),
-    fetch(requests.fetchComedyMovies).then((res) => res.json()),
-    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
-    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
-    fetch(requests.fetchDocumentaries).then((res) => res.json()),
-  ])
+    fetch(requests.fetchNetflixOriginals).then(res => res.json()),
+    fetch(requests.fetchTrending).then(res => res.json()),
+    fetch(requests.fetchTopRated).then(res => res.json()),
+    fetch(requests.fetchActionMovies).then(res => res.json()),
+    fetch(requests.fetchComedyMovies).then(res => res.json()),
+    fetch(requests.fetchHorrorMovies).then(res => res.json()),
+    fetch(requests.fetchRomanceMovies).then(res => res.json()),
+    fetch(requests.fetchDocumentaries).then(res => res.json()),
+  ]);
 
   return {
     props: {
@@ -94,7 +98,6 @@ export const getServerSideProps = async () => {
       horrorMovies: horrorMovies.results,
       romanceMovies: romanceMovies.results,
       documentaries: documentaries.results,
-    }
-  }
-
-}
+    },
+  };
+};
